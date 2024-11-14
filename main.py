ac9f938aae64b7169f4c407b8101379252b1d3cc
Jacob Thomas <thomajac002@warren.k12.in.us>
2024-11-14 10:01:23 -0500

    inital commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#section1
+color_list = ["red", "green", "blue", "yellow"]
+print(color_list[0])
+print(color_list[3])
+#section2
+num_list = [4, 6, 4, 2, 9, 4, 1]
+num_list.append(5)
+print(num_list)
+num_list.remove(4)
+print(num_list.count(4))
+print(num_list.index(9))
+#section 3
+fruit_list = ["apple", "banana", "cherry"]
+print(fruit_list)
+fruit_list[1] = "orange"
+print(fruit_list)
+fruit_list.remove("cherry")
+print(fruit_list)
